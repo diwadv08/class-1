ff9df40d5622146affa0aed5eec2b593b9982d96
Diwakar <you@example.com>
2024-12-11 17:03:10 +0530

    number method

---
+++ number_method.js
@@ -0,0 +1,34 @@
+'use strict'
+var a="1756"; 
+var a1="199.9105aa5a8989833";
+// alert(isNaN(a))              //true
+// alert(isFinite(a))           //false
+
+// alert(a.toFixed())           //1756
+// alert(a.toFixed(1))          //1756.0
+// alert(a.toFixed(2))          //1756.00
+// alert(a.toFixed(3))          //1756.000
+
+// alert(a.toExponential())     //1.756e+3
+// alert(a.toExponential(1))    //1.8e+3
+// alert(a.toExponential(2))    //1.76e+3
+// alert(a.toExponential(3))    //1.756e+3
+// alert(a.toExponential(4))    //1.7560e+3
+// alert(a.toExponential(5))    //1.75600e+3
+
+// alert(a+20)                   //175620
+// alert(Number(a)+20)           //1776
+
+// alert(Number(a1))             //NaN
+// alert(parseInt(a1))           //199
+// alert(parseFloat(a1))         //199.9105
+
+
+// IsFinite 
+// True for Numbers 
+// False for all other datatypes
+
+
+// IsNAN 
+// False for Numbers 
+// True for all other datatypes
