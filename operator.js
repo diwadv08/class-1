73382b59589ca375a8b3ba1233d9359ad60d4f32
Diwakar <you@example.com>
2024-12-13 16:55:15 +0530

    Operator

---
+++ operator.js
@@ -0,0 +1,32 @@
+var a=10;
+var b=2;
+
+
+// Arithmetic Operators
+// document.write(`<h1>The sum of <span>${a}</span> and <span>${b}</span> is <span>${a+b}</span></h1>`)
+// document.write(`<h1>The diffrence of <span>${a}</span> and <span>${b}</span> is <span>${a-b}</span></h1>`)
+// document.write(`<h1>The multiplication of <span>${a}</span> and <span>${b}</span> is <span>${a*b}</span></h1>`)
+// document.write(`<h1>The division of <span>${a}</span> and <span>${b}</span> is <span>${a/b}</span></h1>`)
+// document.write(`<h1>The remainder of <span>${a}</span> and <span>${b}</span> is <span>${a%b}</span></h1>`)
+// document.write(`<h1>The exponential of <span>${a}</span> and <span>${b}</span> is <span>${a**b}</span></h1>`)
+
+// Assignment Operator
+// alert(a+=10)   //20
+// alert(a-10)    //10
+// alert(a+10)    //30
+// alert(a+=10)   //30
+// alert(a)       //30
+
+
+// Comparision Operator
+
+// alert(a==10)  //true
+// alert(a==='10')  //false
+// alert(a!=='10')  //true  
+// alert(a!=10)   //false
+
+
+// Relational Operator
+// alert(a>10)   //false
+// alert(a>=10)   //true
+// alert(a<=10)   //true
