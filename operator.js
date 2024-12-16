62651eb0a49ecc490e97b87f5fc2b662b0283289
Diwakar <you@example.com>
2024-12-16 18:48:44 +0530

    16-12

--- operator.js
+++ operator.js
@@ -1,32 +1,63 @@
-var a=10;
-var b=2;
-
-
-// Arithmetic Operators
-// document.write(`<h1>The sum of <span>${a}</span> and <span>${b}</span> is <span>${a+b}</span></h1>`)
-// document.write(`<h1>The diffrence of <span>${a}</span> and <span>${b}</span> is <span>${a-b}</span></h1>`)
-// document.write(`<h1>The multiplication of <span>${a}</span> and <span>${b}</span> is <span>${a*b}</span></h1>`)
-// document.write(`<h1>The division of <span>${a}</span> and <span>${b}</span> is <span>${a/b}</span></h1>`)
-// document.write(`<h1>The remainder of <span>${a}</span> and <span>${b}</span> is <span>${a%b}</span></h1>`)
-// document.write(`<h1>The exponential of <span>${a}</span> and <span>${b}</span> is <span>${a**b}</span></h1>`)
+// Arithmetic Operator
+// document.write(`<h1>${5+5}</h1>`)
+// document.write(`<h1>${5-5}</h1>`)
+// document.write(`<h1>${5*5}</h1>`)
+// document.write(`<h1>${5/5}</h1>`)  //Quotient
+// document.write(`<h1>${5%5}</h1>`)  //Remainder
+// document.write(`<h1>${8**2}</h1>`)
 
 // Assignment Operator
-// alert(a+=10)   //20
-// alert(a-10)    //10
-// alert(a+10)    //30
-// alert(a+=10)   //30
-// alert(a)       //30
+
+// alert(a+5)
+// alert(a)
+
+// // a=a+10  a+=10
+// // a=a-10  a-=10
+// // a=a*10  a*=10
+// // a=a/10  a/=10
 
 
-// Comparision Operator
-
-// alert(a==10)  //true
-// alert(a==='10')  //false
-// alert(a!=='10')  //true  
-// alert(a!=10)   //false
+// document.write(`<h1>${a+10}</h1>`) 
+// document.write(`<h1>${a}</h1>`) 
 
 
-// Relational Operator
-// alert(a>10)   //false
-// alert(a>=10)   //true
-// alert(a<=10)   //true
+// document.write(`<h1>${a!=="3"}</h1>`)
+// document.write(`<h1>${a==="3"}</h1>`)
+// document.write(`<h1>${a==="10"}</h1>`)
+// document.write(`<h1>${a===10}</h1>`)
+
+
+//Relational Operator
+// document.write(`<h1>${a>13}</h1>`)
+// document.write(`<h1>${a<3}</h1>`)
+// document.write(`<h1>${a<=13}</h1>`)
+// document.write(`<h1>${a>=30}</h1>`)
+
+
+
+// Logical Operator
+// 
+// 
+// 
+//   (AND OR NOT)
+// AND BOth condition && 
+// OR Any One condition  ||
+// Not Opposite for the result  !
+// alert(5===15 && 6===6) 
+// alert(!(5===15))
+
+
+// Ternary Operator
+// (condition)?(true action):(false action)
+
+// (15===15)?alert('A is equal to 5'):alert('A is not equal to 5')
+
+// Unary Operator
+//a++ ,++a ,a-- ,--a
+let a=10;
+alert(a++)   //12  
+alert(--a)
+alert(a++)
+alert(--a)
+alert(a++)
+alert(a)
