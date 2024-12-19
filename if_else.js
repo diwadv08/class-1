0ca9158e85f0caf819eb5da7c3d271b0aa4c51b1
Diwakar <you@example.com>
2024-12-19 16:55:21 +0530

    new

---
+++ if_else.js
@@ -0,0 +1,53 @@
+// var a=50;
+// if(a==50){
+//     alert('A is equal to 50')
+// }
+// else if(a>=50){
+//     alert('A is greater equal to 50')
+// }
+// else if(a<=50){
+//     alert('A is lesser equal to 50')
+// }
+
+// var a="70111881";
+// if(isFinite(a)){
+//     if(a%2==0){
+//         alert(a+' is even number')
+//     }
+//     else if(a%2!=0){
+//         alert(a+' is odd number')
+//     }
+// }
+// else{
+//     alert('a is not a number')
+// }
+
+
+// var a=410;
+// if(isNaN(a)){
+//     alert('A is not a number')
+// }
+// else{
+//     if(a%2==0){
+//         alert(a+' is % by 2')
+//         if(a%5==0){
+//             alert(a+' is % by 5')
+//             if(a%10==0){
+//                 alert(a+' is % by 10')
+//             }
+//             else{
+//                 alert(a+' is not % by 10')
+//             }
+//         }
+//         else{
+//             alert(a+' is % not by 5')
+//         }
+//     }
+//     else{
+//         alert(a+' is not % by 2')
+//     }   
+// }
+
+
+
+
