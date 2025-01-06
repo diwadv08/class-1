900541ae78a1b4bdc81aac543ef4aef2dff2cee6
Diwakar <you@example.com>
2025-01-06 17:04:16 +0530

    06-01

---
+++ array_method.js
@@ -0,0 +1,40 @@
+var a=[40,12,5,10,20];
+
+// a.push(50)
+// a.pop()
+
+a.shift()
+console.log(a);
+
+// for(let i=0;i<a.length;i++){
+//     document.write(a[i]+"---")
+// }
+
+// for(let k of a){
+//     document.write(`<h4>${k}</h4>`);
+// }
+
+
+// var b=a.map((value,index,array)=>{
+//     return value;
+// })
+// var b=a.filter((e)=>{
+//     return e<=10;
+// })
+// var b=a.find((e)=>{
+//     return e<=10;
+// })
+
+// var b=a.find((e)=>{
+//     return e<=10;
+// })
+
+// var b=a.reduce((e,i)=>{
+//     return e+i;
+// })
+
+// console.log(b);
+
+// a.map((e)=>{
+//     document.write(e)
+// })
---
+++ function1.js
@@ -0,0 +1,32 @@
+// function Ab(){
+//    alert(10)
+// }
+// a()
+
+// const a=function(){
+//     alert("Function Alert "+10)
+//     return 15;
+// }
+// alert("Return Value"+b);
+
+
+// const a=()=>{
+//     alert("Function Alert "+10)
+//     return 15;
+// }
+//a()
+
+
+// const tables=()=>{
+//     var box1=document.getElementById("table").value;
+//     var box2=document.getElementById("length").value;
+    
+//     for(let i=1;i<=box1;i++){
+//         for(let j=1;j<=box2;j++){
+//             document.write(`<h4>${j}*${i}=${i*j}</h4>`)
+//         }
+//         document.write(`<hr>`)
+//     }
+// }
+
+
