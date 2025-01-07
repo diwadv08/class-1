9370149fe5e0661b61185604d0bc953ade6deb1a
Diwakar <you@example.com>
2025-01-07 17:12:08 +0530

    07-01

---
+++ card.js
@@ -0,0 +1,36 @@
+// document.write(`<div class="container-fluid">
+//         <div class="container">
+//             <div class="row">`)
+
+// let card_count=4;
+// let colum_count=3;
+// let colors=["","danger","primary","success","warning","secondary","info"]
+// for(i=1;i<=card_count;i++){
+//     document.write(`<div class="col-${colum_count} my-2">
+//         <div class="card ">
+//             <div class="card-body">
+//                 <h1>Hello ${i}</h1>
+//             </div>
+//         </div>
+//         </div>`)
+// }
+
+// document.write(`</div>
+//         </div>
+//     </div>`)
+
+
+// function MyFun(){
+//     var box1=document.getElementById("box1").value;
+//     var box2=document.getElementById("box2").value;
+//     alert(box1+'----'+box2);
+//     for(let i=1;i<=box2;i++){
+//         for(let j=1;j<=box1;j++){
+//             document.write(`<h4>${j}*${i}=${i*j}</h4>`)
+//         }
+//         document.write('<hr>')
+//     }
+//     return false;
+   
+// }
+   
--- funtion2.js
+++ funtion2.js
@@ -1,5 +1,5 @@
-let span=document.getElementById("number");
-let num=Number(span.innerHTML);
+// let span=document.getElementById("number");
+// let num=Number(span.innerHTML);
 
 
 // function Minus(){
@@ -15,12 +15,54 @@ let num=Number(span.innerHTML);
 //     }
 // }
 
-function inc(){
-    if(num<500){
-        num+=1;
-        span.innerHTML=num;
-    }
+// function inc(){
+//     if(num<500){
+//         num+=1;
+//         span.innerHTML=num;
+//     }
    
-}
+// }
 
-setInterval(inc,0)
+// setInterval(inc,0)
+
+
+// var date=new Date();
+// alert(date.getFullYear())
+// alert(date.getMonth())
+// alert(date.getDate())
+// alert(date.getDay())
+// alert(date.getHours())
+// alert(date.getMinutes())
+// alert(date.getSeconds())
+
+
+
+// function A1(a){
+//     alert("Hi "+a+"!")
+// }
+
+// A1('Harish')
+// A1('Suresh')
+// A1('Vijay')
+// A1('Varun')
+// A1('Sanjay')
+
+
+// function cal(a=1,b=1){
+//     document.write(`<h1>${a+b}</h1>`)
+//     document.write(`<h1>${a-b}</h1>`)
+//     document.write(`<h1>${a*b}</h1>`)
+//     document.write(`<h1>${a/b}</h1>`)
+//     document.write(`<hr>`)
+// }
+
+// cal()
+
+
+
+// function A1(){
+//     alert(0)
+// }
+// A1()
+
+
