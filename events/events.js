c6530534c2bd252167504af7c9643c6ca3bafe85
Diwakar <you@example.com>
2025-01-22 17:10:42 +0530

    22-01

---
+++ events/events.js
@@ -0,0 +1,19 @@
+let count=1;
+const Move=()=>{
+    console.log("Mouse moved-"+count++)
+}
+const Hover=()=>{
+    console.log("Mouse moved-"+count++)
+}
+
+
+
+const Up=(e)=>{
+    e.style.background='green';
+    e.innerHTML='Mouse Up';
+
+}
+const Down=(e)=>{
+    e.style.background='blue';
+    e.innerHTML='Mouse Downed';
+}
--- filter/script.js
+++ filter/script.js
@@ -1,3 +1,5 @@
+let divTable=document.querySelector(".divTable");
+let input=document.querySelector("input");
 const data=[
     {
         id:1,
@@ -44,19 +46,23 @@ const data=[
 function Def(){
     const thead=Object.keys(data[0]);
     let table_data=`<table cellpadding='20'><tr class='tcss'>`;
+    thead.unshift('S.No')
     thead.map((e)=>{
         table_data+=`<th>${e}</th>`;    
     })
+
     table_data+='</tr>';
+    let s_no=1;
     data.map((e)=>{
         table_data+=`<tr>
-                        <td>${e.id}</td>
+                        <td>${s_no++}</td>
+                        <td>User-${e.id}</td>
                         <td>${e.name}</td>
                         <td>${e.age}</td>
                         <td>${e.from}</td>
                     </tr>`
     })
-    document.body.innerHTML+=table_data;
+    divTable.innerHTML=table_data;
     let table=document.querySelector("table");
     let tcss=document.querySelector(".tcss");
     table.style.width='100%';
@@ -65,9 +71,45 @@ function Def(){
     tcss.style.textTransform='uppercase';
 }
 
+function Typing(e){
+    let myValue=e.target.value;   
+    var final_tabledata=data.filter((data)=>{
+       if(data.name.toLowerCase().startsWith(myValue.toLowerCase())){
+            return data;
+       }    
+    })
+    const thead=Object.keys(data[0]);
+    if(final_tabledata.length>0){
+        let table_data=`<table cellpadding='20'><tr class='tcss'>`;
+        thead.unshift('S.No')
+        thead.map((e)=>{
+            table_data+=`<th>${e}</th>`;    
+        })
+        let s_no=1;
+        table_data+='</tr>';
+        final_tabledata.map((e)=>{
+            table_data+=`<tr>
+                            <td>${s_no++}</td>
+                            <td>User-${e.id}</td>
+                            <td>${e.name}</td>
+                            <td>${e.age}</td>
+                            <td>${e.from}</td>
+                        </tr>`
+        })        
+        divTable.innerHTML=table_data;
+        let table=document.querySelector("table");
+        let tcss=document.querySelector(".tcss");
+        table.style.width='100%';
+        tcss.style.background='#e3e3e3';
+        tcss.style.textAlign='left';
+        tcss.style.textTransform='uppercase';
+    }
+    else{
+        divTable.innerHTML=`<center>No results found for "${myValue}"</center>`;
+    }
+}
 
-
-
+input.addEventListener('keyup',Typing)
 window.addEventListener('load',Def);
 
 
