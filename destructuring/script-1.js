9461a77598c7618a2ee862df80ea17854c6202ba
Diwakar <you@example.com>
2025-01-30 17:00:16 +0530

    30-01

---
+++ destructuring/script-1.js
@@ -0,0 +1,43 @@
+// let arr=[10,20,30,40,50,60];
+
+// let a=arr[0]
+// let b=arr[1]
+// let c=arr[2]
+// let d=arr[3]
+// let e=arr[4]
+// let[a,b,...c]=arr;
+
+
+// let a={
+//     name:'Hari',
+//     age:12,
+//     pwd:123
+// }
+// // let name=a.name;
+
+// let {name,age,pwd}=a;
+
+
+// console.log(name);
+// console.log(age);
+// console.log(pwd);
+
+
+// let word="Hello World Welcome";
+// let arr=word.split(' World ');
+
+// console.log(arr);
+
+
+// let string="dlrow";
+
+// let arr=string.split('');
+// let text='';
+// for(let i=arr.length-1;i>=0;i--){
+//     text+=arr[i];
+// }
+// console.log(text);
+
+// window.addEventListener('resize',()=>{
+//     console.log(window.innerWidth+"--"+window.innerHeight);    
+// })
---
+++ destructuring/script.js
@@ -0,0 +1,49 @@
+
+// function area1(a,v,d1,d){
+
+// }
+// var area2=function (){
+
+// }; 
+
+
+// var area3=function hh(){
+
+// }; 
+// document.write(area1.length)
+// document.write("<br>");  
+// document.write(area1.name)
+// document.write("<br>");                       
+// document.write(area2.name)
+// document.write("<br>");                      
+// document.write(area3.name)
+// document.write("<br>");    
+
+
+    //   function user(brandname,location,year){      
+    //     return brandname+'---'+location+'---'+year;
+    //   }
+      
+    //  document.write(user.a="ILIFE");
+    //  document.write("<br>");
+    //  document.write(user.b="Noida");
+    //  document.write("<br>");
+    //  document.write(user.c=2012);
+    //  document.write("<br>");
+    //  document.write(user.d=12);
+    //  document.write("<br>");
+    
+    //  document.write(user(1,5,10));
+
+
+  function fun1(){
+    return 10;
+  }
+  function fun2(){
+    return 5;
+  }
+  function fun3(){
+    return fun1()+fun2();
+  }
+  alert(fun3());
+  
